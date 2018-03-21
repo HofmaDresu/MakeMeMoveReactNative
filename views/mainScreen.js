e0619dc0ce31aeed63a64c6e21995e864513be6a
Matt Hoffman <hofmadresu@gmail.com>
2018-03-21 14:57:49 -0400

    Add icons to tab bar

--- views/mainScreen.js
+++ views/mainScreen.js
@@ -1,21 +1,33 @@
 
 import React from 'react';
-import {  TabNavigator } from 'react-navigation';
+import {  TabNavigator, TabBarBottom } from 'react-navigation';
 import  ScheduleScreen from './schedule';
 import  ExercisesScreen from './exercises';
 import { PrimaryColor, HeaderTintColor } from '../colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const TabStack = TabNavigator({
     MySchedule: {screen: ScheduleScreen},
     MyExercises: {screen: ExercisesScreen},
 },
 {
+    navigationOptions: ({navigation}) =>({
+        tabBarIcon: () => {
+            const { routeName } = navigation.state;
+            const iconName = routeName === "MySchedule" ? "calendar" : "navicon";
+            return (<Icon name={iconName} size={25} color={HeaderTintColor} />);
+        },
+    }),
     tabBarOptions: {
         activeTintColor: HeaderTintColor,
         style: {
             backgroundColor: PrimaryColor,
         },
+        showIcon: true,
     },
+    tabBarComponent: TabBarBottom,
+    animationEnabled: true,
+    swipeEnabled: false,
 });
 
 export default class MySchedule extends React.Component {
